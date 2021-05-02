50bba2bcda936c2d0941152b35d61c1a806a2968
Abdala <hmabdala@gmail.com>
2021-05-02 15:58:56 -0300

    inserindo correcao TxtLoader

--- src/main/java/br/com/fhi/fiapcredito/config/AlunoTxtLoader.java
+++ src/main/java/br/com/fhi/fiapcredito/config/AlunoTxtLoader.java
@@ -39,45 +39,50 @@ public class AlunoTxtLoader implements ApplicationRunner {
 		return Paths.get(getClass().getClassLoader().getResource("lista_alunos.txt").toURI());
 	}
 
+	public String gerarIdMoip(String ra, String nome) {
+    	String token = "QHSDKTLIMA7MTNCLZWAFXSLNUTDCSKIZ";
+    	String key = "7JP4ONJAYNFNYSKGM9ZQVCNV2UYCLKBUBAG1WFQS";
+    	Authentication auth = new BasicAuth(token, key);
+    	Setup setup = new Setup().setAuthentication(auth).setEnvironment(Setup.Environment.SANDBOX);
+    	Map<String, Object> taxDocument = payloadFactory(
+    	        value("type", "CPF"),
+    	        value("number", "10013390023")
+    	);
+    	Map<String, Object> phone = payloadFactory(
+    	        value("countryCode", "55"),
+    	        value("areaCode", "11"),
+    	        value("number", "22226842")
+    	);
+    	Map<String, Object> customerRequestBody = payloadFactory(
+    	        value("ownId", ra),
+    	        value("fullname", nome),
+    	        value("email", "test.moip@mail.com"),
+    	        value("birthDate", "1980-5-10"),
+    	        value("taxDocument", taxDocument),
+    	        value("phone", phone)
+    	);
+    	Map<String, Object> responseCreation = Moip.API.customers().create(customerRequestBody, setup);
+		return responseCreation.get("id").toString();
+	}
+	
 	@Override
 	public void run(ApplicationArguments args) throws Exception {
 
 		try (Stream<String> lines = Files.lines(getFilePath())) {
 			lines.filter(line -> !line.startsWith("-")).filter(line -> !line.isBlank())
 					.filter(line -> line.length() == 55).forEach(line -> {
-						AlunoDTO novoAluno = new AlunoDTO();
-						novoAluno.setNomeAluno(line.substring(0, 40).replaceAll("\\s+$", ""));
-						novoAluno.setRegistroAluno(line.substring(41, 48));
-						novoAluno.setCodigoCartao(line.substring(50, 55));
-				    	String token = "QHSDKTLIMA7MTNCLZWAFXSLNUTDCSKIZ";
-				    	String key = "7JP4ONJAYNFNYSKGM9ZQVCNV2UYCLKBUBAG1WFQS";
-				    	Authentication auth = new BasicAuth(token, key);
-				    	Setup setup = new Setup().setAuthentication(auth).setEnvironment(Setup.Environment.SANDBOX);
-				    	Map<String, Object> taxDocument = payloadFactory(
-				    	        value("type", "CPF"),
-				    	        value("number", "10013390023")
-				    	);
-				    	Map<String, Object> phone = payloadFactory(
-				    	        value("countryCode", "55"),
-				    	        value("areaCode", "11"),
-				    	        value("number", "22226842")
-				    	);
-				    	Map<String, Object> customerRequestBody = payloadFactory(
-				    	        value("ownId", novoAluno.getRegistroAluno()),
-				    	        value("fullname", novoAluno.getNomeAluno()),
-				    	        value("email", "test.moip@mail.com"),
-				    	        value("birthDate", "1980-5-10"),
-				    	        value("taxDocument", taxDocument),
-				    	        value("phone", phone)
-				    	);
-				    	Map<String, Object> responseCreation = Moip.API.customers().create(customerRequestBody, setup);
-				    	novoAluno.setIdMoip(responseCreation.get("id").toString());
+						String nome = line.substring(0, 40).replaceAll("\\s+$", "");
+						String ra = line.substring(41, 48);
+						String codigoCartao = line.substring(50, 55);
+						String idMoip = gerarIdMoip(ra, nome);
+						AlunoDTO novoAluno = new AlunoDTO(nome, ra, codigoCartao, idMoip);
 						alunoService.criarAluno(novoAluno);
 					});
 
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
+		
 
 	}
 }
