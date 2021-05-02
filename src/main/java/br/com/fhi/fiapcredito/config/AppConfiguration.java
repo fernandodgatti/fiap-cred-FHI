6b4420465ca867d303564588f8c7637c239d14d1
Abdala <hmabdala@gmail.com>
2021-05-02 19:42:44 -0300

    subindo swagger

--- src/main/java/br/com/fhi/fiapcredito/config/AppConfiguration.java
+++ src/main/java/br/com/fhi/fiapcredito/config/AppConfiguration.java
@@ -2,9 +2,14 @@ package br.com.fhi.fiapcredito.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.RestController;
 
 import br.com.fhi.fiapcredito.validator.AlunoValidator;
 import br.com.fhi.fiapcredito.validator.AlunoValidatorImpl;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
 public class AppConfiguration {
@@ -12,5 +17,13 @@ public class AppConfiguration {
 	@Bean
 	public AlunoValidator alunoValidator() {
 		return new AlunoValidatorImpl();
+	}
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+        .select()
+        .apis(RequestHandlerSelectors.withClassAnnotation(RestController.class))
+        .paths(PathSelectors.any())
+        .build();
 	}
 }
